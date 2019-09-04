261ea066c15a797ec0f5cabb13a4f3b875304607
Your Name <you@example.com>
2019-09-04 17:41:39 +0530

    gemshein template

--- bin/index.js
+++ bin/index.js
@@ -1,10 +1,13 @@
-var http = require('http');
 
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, { "Content-type": "text/plain" });
-    res.end("Hello world\n");
-});
-
-server.listen(3000, function() {
-    console.log('Server is running at 3000')
+var http = require('http'),
+fs = require('fs');
+fs.readFile('./bin/index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(3000);
 });
